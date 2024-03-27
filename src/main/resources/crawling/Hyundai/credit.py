4748b72daafde9383fc42179b471bdd9a00ff17a
julia-heo <100216331+julia-heo@users.noreply.github.com>
2024-03-28 00:12:24 +0900

    안바껴

--- src/main/resources/crawling/Hyundai/credit.py
+++ src/main/resources/crawling/Hyundai/credit.py
@@ -5,7 +5,6 @@ from datetime import datetime
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
 
 '''
